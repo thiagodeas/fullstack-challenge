b3d8a923f82f2e1e309dbd9319d1940458a52f70
thiagodeas <sousaalvesth@gmail.com>
2025-10-15 17:33:44 -0300

    chore(services): enable global ValidationPipe (auth-service, tasks-service)

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -2,9 +2,18 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidUnknownValues: true,
+      transformOptions: { enableImplicitConversion: true }
+    })
+  );
 
   const rabbitUrl = process.env.RABBITMQ_URL || 'amqp://admin:admin@localhost:5672';
   app.connectMicroservice<MicroserviceOptions>({
